7185f45445890cdacb3871467529752215a3fb6d
Vikas Bhandari <143922604+Vikaskumarrr@users.noreply.github.com>
2024-10-08 19:16:40 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,162 +1,77 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const mongoDbSession = require("connect-mongodb-session")(session);
+require("dotenv").config();
+const mongoose = require("mongoose")
 
 //file-imports
-const userModel = require("./userSchema");
+const { userDataValidation} = require("./utils/authUtil")
+const userModel = require("./models/userModel")
 
-//contant
+// constants
 const app = express();
-const MONGO_URL = "mongodb+srv://vikas:vikas@cluster0.q8ksl.mongodb.net/augTestDB"
+const PORT = process.env.PORT;
 
-const store = new mongoDbSession({ 
-    url : MONGO_URL,
-    collection : "sessions",
+//db connection
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("mongoDb connected sucesfully.."))
+.catch((err)=> console.log(err));
 
-})
+//Middlewares
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());  
 
-// Middleware
-app.use(express.urlencoded({ extended: true })); // Global middleware
-app.use(express.json()); // JSON body parser 
-
-app.use(
-    session({    
-    secret: "This is aug backend module",  
-    store : store,
-    resave : false,
-    saveUninitialized : false , 
-    }) 
-);
-
-// db connection 
-
-mongoose.connect(MONGO_URL).then(() => {
-    console.log("mongoose connected sucessfully");
-}).catch((err) => {
-    console.log(err);
+// API's
+app.get('/',(req,res)=>{ 
+    return res.send("Server is up and running...")
 })
 
 
-app.get("/", (req, res) => {
-    return res.send("Server is running ")
+app.get('/register',(req,res)=>{ 
+    return res.render("registerPage");
 })
 
-//register form
-app.get("/register-form", (req , res) => {
-    return res.send(`
-        <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Register Form</h1>
-    <form action="/register-user" method="POST">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name"><br>
-        <label for="email">email:</label>
-        <input type="email" id="email" name="email"><br>
-        <label for="password">password:</label>
-        <input type="password" id="password" name="password"><br>
-        <button type="submit">submit</button>
-    </form>
-</body>
-</html> `);
-})
-
-app.post("/register-user", async (req, res) => {
+app.post('/register', async (req,res)=>{ 
     console.log(req.body);
 
-    const { name, email, password } = req.body;
+    const {name, username , password} = req.body
 
-    try {
-        const userDb = await userModel.create({
-            //schema : client
-            name: name,
-            email: email,
-            password: password,
-        });
+    //data validation
+    try { 
+        await userDataValidation({name , username, password});
+    } 
+    catch(error){ 
+        return res.status(400).json(error);
+    }
+
+    const userObj = new userModel({
+        name: name,
+        username: username,
+        password: password
+    });
+
+    try { 
+        const userDb = await userObj.save();
 
         return res.status(201).json({
-            message: "User created sucessfully",
-            data: userDb
+            message: "Register Successfull.",
+            data: userDb,
         });
-    } catch (error) {
-        return res.status(500).json({
-            message: "Internal server error",
-            error: error
-        });
-    }
-});
-
-//login form
-app.get("/login-form", (req, res)=>{ 
-    return res.send(`
-        <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Login Form</h1>
-    <form action="/login-user" method="POST">
-        <label for="email">email:</label>
-        <input type="email" id="email" name="email"><br>
-        <label for="password">password:</label>
-        <input type="password" id="password" name="password"><br>
-        <button type="submit">submit</button>
-    </form>
-</body>
-</html> `);
-})
-
-app.post("/login-user" , async (req, res)=>{ 
-
-    // find the user with email
-    // compare the password
-    // seesion base authantication
-
-    console.log(req.body)
-    try{ 
-        const userDb = await userModel.findOne({email : req.body.email})
-
-        // if user not found
-        if(!userDb)
-        { 
-            return res.status(400).json("User not found please register first")
-        }
-
-        // compare password
-
-        if(userDb.password !== req.body.password){ 
-            return res.status(400).json("Incorrect password")
-        }
-
-        // session 
-        console.log(req.session);
-        req.session.isAuth = ture ;
-
-
-        return res.status(200).json("Login succesfully")
-    } catch(error)
-    { 
-        return res.status(500).json({
-            message: "Internal server error",
-            error: error
-     });
+    } catch(error){ 
+        return res.status(500).json({ 
+            message:"Internal server error",
+            error: error,
+        })
     }
 
+    return res.send("all Ok..")
 })
 
-    app.get('/dashboard', (req , res)=>{ 
-        return res.send("Dashboard Page!!!!!");
-    })
+app.get('/login',(req,res)=>{ 
+    return res.render("loginPage");
+})
 
-app.listen(8000, () => {
-    console.log("server is runnig PORT:8000")
+
+
+app.listen(PORT, ()=>{ 
+    console.log(`server is runnig at http://localhost:${PORT}`);
 })
