7b27e1b37b1a89e95753d06dc5bd911973ffb28e
Vikas Bhandari <143922604+Vikaskumarrr@users.noreply.github.com>
2024-10-24 22:40:25 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,77 +1,29 @@
+
 const express = require("express");
-require("dotenv").config();
-const mongoose = require("mongoose")
 
-//file-imports
-const { userDataValidation} = require("./utils/authUtil")
-const userModel = require("./models/userModel")
-
-// constants
 const app = express();
-const PORT = process.env.PORT;
 
-//db connection
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("mongoDb connected sucesfully.."))
-.catch((err)=> console.log(err));
+const server = require('http').Server(app);
 
-//Middlewares
-app.set('view engine','ejs');
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());  
+const all = require('socket.io')(server);
 
-// API's
-app.get('/',(req,res)=>{ 
-    return res.send("Server is up and running...")
-})
+app.use(express.static('public'));
 
+all.on('connection', (conection)=>{ 
+    console.log('User connected', conection.id);
 
-app.get('/register',(req,res)=>{ 
-    return res.render("registerPage");
-})
-
-app.post('/register', async (req,res)=>{ 
-    console.log(req.body);
-
-    const {name, username , password} = req.body
-
-    //data validation
-    try { 
-        await userDataValidation({name , username, password});
-    } 
-    catch(error){ 
-        return res.status(400).json(error);
-    }
-
-    const userObj = new userModel({
-        name: name,
-        username: username,
-        password: password
+    conection.on('messageSent',(data)=>{ 
+        all.emit('messageSent', data);
     });
 
-    try { 
-        const userDb = await userObj.save();
-
-        return res.status(201).json({
-            message: "Register Successfull.",
-            data: userDb,
-        });
-    } catch(error){ 
-        return res.status(500).json({ 
-            message:"Internal server error",
-            error: error,
-        })
-    }
-
-    return res.send("all Ok..")
-})
-
-app.get('/login',(req,res)=>{ 
-    return res.render("loginPage");
-})
+    conection.on("joined", (username)=>{ 
+        all.emit("joined", username)
+    })
+});
 
 
+const PORT = 3000;
+server.listen(PORT, ()=>{ 
+    console.log(`Server is running on post http://127.0.01: ${PORT}`)
+});
 
-app.listen(PORT, ()=>{ 
-    console.log(`server is runnig at http://localhost:${PORT}`);
-})
